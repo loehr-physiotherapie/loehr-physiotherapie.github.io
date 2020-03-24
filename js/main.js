6603f36661f862304b1d765c737cddbdf224539a
nirnai <rnirnai@gmail.com>
2020-03-24 13:30:56 +0100

    changed datenschutz style and added correct phone number

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
+const firstSection = document.querySelector(".firstSection");
 const mainLogo = document.querySelector(".main-logo");
 const navLogo = document.querySelector(".nav-logo");
 
@@ -13,21 +14,21 @@ observer = new IntersectionObserver((entries) => {
     // if(! (entries[0].isIntersecting ))
     if(entries[0].intersectionRatio < 0.5)
     {   
-        mainLogo.style.opacity = 0.5;
+        firstSection.style.opacity = 0.5;
         navLogo.classList.add("nav-logo");
         navLogo.classList.remove("nav-logo-hidden");
         header.classList.add("nav-scroll")
     }
     else
     {
-        mainLogo.style.opacity = 1;
+        firstSection.style.opacity = 1;
         navLogo.classList.add("nav-logo-hidden");
         navLogo.classList.remove("nav-logo");
         header.classList.remove("nav-scroll")
     }
 }, config)
 
-observer.observe(mainLogo);
+observer.observe(firstSection);
 
 $('.contact-check').on('change', function() {
     $('.contact-check').not(this).prop('checked', false);  
