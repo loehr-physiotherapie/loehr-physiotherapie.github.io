e11d4af8ef6fe0721ab6715bcb1039521e6b88b6
nirnai <rnirnai@gmail.com>
2020-06-07 14:07:12 +0200

    cards have only background color

--- js/main.js
+++ js/main.js
@@ -3,30 +3,35 @@ const firstSection = document.querySelector(".firstSection");
 const mainLogo = document.querySelector(".main-logo");
 const navLogo = document.querySelector(".nav-logo");
 
-const config =
-{
-    rootMargin: "-400px 0px 0px 0px",
-    threshold: "0.5"
-};
+var window_width = window.matchMedia("(min-width: 1440px)")
+if (window_width.matches) {
+    const config =
+    {
+        rootMargin: "-400px 0px 0px 0px",
+        threshold: "0.5"
+    };
 
-observer = new IntersectionObserver((entries) => {
-    console.log(entries)
-    // if(! (entries[0].isIntersecting ))
-    if (entries[0].intersectionRatio < 0.5) {
-        firstSection.style.opacity = 0.5;
-        navLogo.classList.add("nav-logo");
-        navLogo.classList.remove("nav-logo-hidden");
-        header.classList.add("nav-scroll")
-    }
-    else {
-        firstSection.style.opacity = 1;
-        navLogo.classList.add("nav-logo-hidden");
-        navLogo.classList.remove("nav-logo");
-        header.classList.remove("nav-scroll")
-    }
-}, config)
+    observer = new IntersectionObserver((entries) => {
+        console.log(entries)
+        // if(! (entries[0].isIntersecting ))
+        if (entries[0].intersectionRatio < 0.5) {
+            firstSection.style.opacity = 0.5;
+            navLogo.classList.add("nav-logo");
+            navLogo.classList.remove("nav-logo-hidden");
+            header.classList.add("nav-scroll")
+        }
+        else {
+            firstSection.style.opacity = 1;
+            navLogo.classList.add("nav-logo-hidden");
+            navLogo.classList.remove("nav-logo");
+            header.classList.remove("nav-scroll")
+        }
+    }, config)
 
-observer.observe(firstSection);
+
+    observer.observe(firstSection);
+}
+
 
 $('.contact-check').on('change', function () {
     $('.contact-check').not(this).prop('checked', false);
