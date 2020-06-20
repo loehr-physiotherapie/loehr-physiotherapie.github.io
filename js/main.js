14bd20bd3a975178789b59e68e5c5c22d6de83dc
nirnai <rnirnai@gmail.com>
2020-06-20 15:19:47 +0200

    fixed header and intersection observer

--- js/main.js
+++ js/main.js
@@ -5,30 +5,34 @@ const navLogo = document.querySelector(".nav-logo");
 
 var window_width = window.matchMedia("(min-width: 1440px)")
 if (window_width.matches) {
-    const config =
+    const options =
     {
-        rootMargin: "-400px 0px 0px 0px",
-        threshold: "0.5"
+        root: document.main,
+        rootMargin: "0px 0px 0px 0px",
+        threshold: [...Array(100).keys()].map(x => x / 100)
     };
-
-    observer = new IntersectionObserver((entries) => {
-        console.log(entries)
-        // if(! (entries[0].isIntersecting ))
-        if (entries[0].intersectionRatio < 0.5) {
-            firstSection.style.opacity = 0.5;
-            navLogo.classList.add("nav-logo");
-            navLogo.classList.remove("nav-logo-hidden");
-            header.classList.add("nav-scroll")
+    function callback(entries, observer) {
+        const ratio = entries[0].intersectionRatio;
+        const boundingRect = entries[0].boundingClientRect;
+        const intersectionRect = entries[0].intersectionRect;
+        // if (entries[0].isIntersecting) {
+        // if (entries[0].intersectionRatio < 0.5) {
+        if (ratio < 1) {
+            if (boundingRect.top < intersectionRect.top) {
+                firstSection.style.opacity = 0.5;
+                navLogo.classList.add("nav-logo");
+                navLogo.classList.remove("nav-logo-hidden");
+                header.classList.add("nav-scroll")
+            }
+            else {
+                firstSection.style.opacity = 1;
+                navLogo.classList.add("nav-logo-hidden");
+                navLogo.classList.remove("nav-logo");
+                header.classList.remove("nav-scroll")
+            }
         }
-        else {
-            firstSection.style.opacity = 1;
-            navLogo.classList.add("nav-logo-hidden");
-            navLogo.classList.remove("nav-logo");
-            header.classList.remove("nav-scroll")
-        }
-    }, config)
-
-
+    }
+    let observer = new IntersectionObserver(callback, options)
     observer.observe(firstSection);
 }
 
